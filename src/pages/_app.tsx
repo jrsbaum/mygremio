69c977cf92864bb252d03c8d18be5c6ccc127cef
Júnior Baum <juniorsbaum@hotmail.com>
2022-12-06 00:48:24 -0300

    feat: home

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useLayoutEffect, useState } from "react";
+import { Loader } from "./Loader";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const handleTransitionEnd = () => {
+    console.log("animation end!");
+    const root = document.getElementById("__next");
+    const loader = document.getElementById("loader");
+    root?.removeChild(loader!);
+  };
+
+  useLayoutEffect(() => {
+    setTimeout(() => setIsLoading(false), 1400);
+  }, []);
+
+  return (
+    <>
+      <Loader onTransitionEnd={handleTransitionEnd} isLoading={isLoading} />
+      <Component {...pageProps} />
+    </>
+  );
+}
